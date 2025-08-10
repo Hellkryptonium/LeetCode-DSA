845efe4ad8ea9c5671b24e5a88afd08e9d3296ba
Mohd Harish <harishjs1006@gmail.com>
2025-08-10 10:11:23 +0530

    Time: 10 ms (5.32%), Space: 33.2 MB (6.55%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-        int n = nums.size();
-        int left = 0, sum = 0;
-        int minLen = INT_MAX;
-
-        for(int right=0; right<n; right++) {
-            sum += nums[right];
-
-            while(sum >= target) {
-                minLen = min(minLen, right - left + 1);
-                sum -= nums[left];
-                left++;
+    int binarySearch(const vector<int>& prefix, int start, int target) {
+        int left = start, right = prefix.size() - 1;
+        int ans = -1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(prefix[mid] >= target) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
+        return ans;
+    }
+
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n+1, 0);
+
+        for(int i=0; i<n; i++) {
+            prefix[i+1] = prefix[i] + nums[i];
+        }
+
+        int minLen = INT_MAX;
+
+        for(int i=0; i<n; i++) {
+            int required = target + prefix[i];
+            int j = binarySearch(prefix, i+1, required);
+            if(j != -1) {
+                minLen = min(minLen, j-i);
+            }
+        }
+
         return (minLen == INT_MAX) ? 0 : minLen;
     }
 };
