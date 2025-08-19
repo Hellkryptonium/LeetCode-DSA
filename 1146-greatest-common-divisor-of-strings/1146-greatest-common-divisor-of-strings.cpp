12f87a1521c5763c16a64d5c0f5294a476c7156c
Mohd Harish <harishjs1006@gmail.com>
2025-08-19 20:56:27 +0530

    Time: 0 ms (100%), Space: 9.6 MB (91.69%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
-    string gcdOfStrings(string str1, string str2) {
-        if(str1 + str2 != str2 + str1) return "";
+    bool divides(const string &s, const string &t) {
+        if(s.length() % t.length() != 0) return false;
+        int repeat = s.length() / t.length();
+        string temp = "";
+        for(int i=0; i<repeat; i++) {
+            temp += t;
+        }
+        return temp == s;
+    }
 
+    string gcdOfStrings(string str1, string str2) {
         int lenGCD = gcd(str1.length(), str2.length());
-        return str1.substr(0,lenGCD);
+        string candidate = str1.substr(0, lenGCD);
+        if(divides(str1, candidate) && divides(str2, candidate)) {
+            return candidate;
+        } else {
+            return "";
+        }
     }
 };
