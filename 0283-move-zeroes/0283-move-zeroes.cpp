9e3ae930215db5419dd5ea344adcc74cef84ed99
Mohd Harish <harishjs1006@gmail.com>
2025-08-29 19:21:19 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.2%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastZeroIndex = 0;
+
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] != 0) {
+                nums[lastZeroIndex] = nums[i];
+                lastZeroIndex++;
+            }
+        }
+
+        for(int i=lastZeroIndex; i<nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
