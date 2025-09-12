eac5d680983af9a093696f9610f2616a7f55bf77
Mohd Harish <harishjs1006@gmail.com>
2025-09-12 17:58:21 +0530

    Time: 6 ms (7.56%), Space: 9.9 MB (57.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int st=0, end=s.length()-1;
-
+        int st=0, end = s.length()-1;
         while(st < end) {
             if(!isalnum(s[st])) {
-                st++; continue;
+                st++;
+                continue;
             }
             if(!isalnum(s[end])) {
-                end--; continue;
+                end--;
+                continue;
             }
-            if(tolower(s[st]) != tolower(s[end])) {
+            if(tolower(s[st++]) != tolower(s[end--])) {
                 return false;
             }
-            st++; end--;
         }
         return true;
     }
