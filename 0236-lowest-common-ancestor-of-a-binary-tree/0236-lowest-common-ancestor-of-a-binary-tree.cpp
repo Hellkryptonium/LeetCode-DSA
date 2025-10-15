3875dd20cf5040900d6c99dd7974be8fc1e65596
Mohd Harish <harishjs1006@gmail.com>
2025-10-15 12:10:30 +0530

    Time: 14 ms (32.59%), Space: 20.4 MB (5.3%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,17 +10,36 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == nullptr || root==p || root==q) {
-            return root;
+        unordered_map<TreeNode*, TreeNode*> parent;
+        stack<TreeNode*> st;
+        parent[root] = nullptr;
+        st.push(root);
+
+        while(parent.find(p) == parent.end() || parent.find(q) == parent.end()) {
+            TreeNode* node = st.top();
+            st.pop();
+
+            if(node->left) {
+                parent[node->left] = node;
+                st.push(node->left);
+            }
+
+            if(node->right) {
+                parent[node->right] = node;
+                st.push(node->right);
+            }
         }
 
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-
-        if(left && right) {
-            return root;
+        unordered_set<TreeNode*> ancestor;
+        while(p != nullptr) {
+            ancestor.insert(p);
+            p = parent[p];
         }
 
-        return left ? left : right;
+        while(ancestor.find(q) == ancestor.end()) {
+            q = parent[q];
+        }
+
+        return q;
     }
 };
