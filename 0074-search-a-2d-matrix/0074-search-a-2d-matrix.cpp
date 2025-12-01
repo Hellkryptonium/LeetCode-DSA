12d8dddabd431a47bbeb26a780244e16a613b307
Mohd Harish <harishjs1006@gmail.com>
2025-12-01 18:33:33 +0530

    Time: 0 ms (100%), Space: 13.3 MB (75.29%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,14 +5,14 @@ public:
         int m = mat[0].size();
 
         for(int i=0; i<n; i++) {
-            int st=0, end = m-1;
-            while(st <= end) {
+            int st=0, end=m-1;
+            while(st<=end) {
                 int mid = st + (end-st)/2;
                 if(mat[i][mid] == key) return true;
-                else if(mat[i][mid] <= key) {
-                    st = mid + 1;
+                else if(mat[i][mid] < key) {
+                    st = mid+1;
                 } else {
-                    end = mid - 1;
+                    end = mid-1;
                 }
             }
         }
