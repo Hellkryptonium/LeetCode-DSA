33b1ba210156f750d6af4809fba8bef589ec0064
Mohd Harish <harishjs1006@gmail.com>
2025-12-02 20:42:57 +0530

    Time: 0 ms (100%), Space: 15.2 MB (32.38%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,25 +1,23 @@
 class Solution {
-private: 
+private:
     int searchRotated(vector<int>& nums, int si, int ei, int target) {
-        if(si > ei) {
-            return -1;
-        }
+            if(si > ei) return -1;
 
-        int mid = si + (ei - si) / 2;
-        if(nums[mid] == target) return mid;
-        if(nums[si] <= nums[mid]) {//L1
-            if(nums[si] <= target && target <= nums[mid]) { // left
-                return searchRotated(nums, si, mid-1, target);
-            } else { // right
-                return searchRotated(nums, mid+1, ei, target);
+            int mid = si + (ei - si)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[si] <= nums[mid]) {//L1
+                if(nums[si] <= target && target <= nums[mid]) {//Left
+                    return searchRotated(nums, si, mid-1, target);
+                } else {//right
+                    return searchRotated(nums, mid+1, ei, target);
+                }
+            } else {
+                if(nums[mid] <= target && target <= nums[ei]) {//right
+                    return searchRotated(nums, mid+1, ei, target);
+                } else {
+                    return searchRotated(nums, si, mid-1, target);
+                }
             }
-        } else {//L2
-            if(nums[mid] <= target && target <= nums[ei]) { //right
-                return searchRotated(nums, mid+1, ei, target);
-            } else { // left
-                return searchRotated(nums, si, mid-1, target);
-            }
-        }
     }
 public:
     int search(vector<int>& nums, int target) {
