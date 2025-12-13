e61648869995455290c626f82e6db7aa095cc121
Mohd Harish <harishjs1006@gmail.com>
2025-12-13 16:22:29 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return nums[0];
+        if(n == 1) return nums[0];
 
         int prev2 = nums[0];
         int prev1 = max(nums[0], nums[1]);
@@ -16,7 +16,6 @@ public:
             prev2 = prev1;
             prev1 = curr;
         }
-
         return prev1;
     }
 };
