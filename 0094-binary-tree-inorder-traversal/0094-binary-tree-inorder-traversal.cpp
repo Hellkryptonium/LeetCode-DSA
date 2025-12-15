7a107f7e8835e220f6ba64f0102685bd2a159d4d
Mohd Harish <harishjs1006@gmail.com>
2025-12-15 10:50:12 +0530

    Time: 3 ms (3.19%), Space: 10.9 MB (33.19%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,18 +10,16 @@
  * };
  */
 class Solution {
-    vector<int> result;
-private:
-    void dfs(TreeNode* node) {
-        if(!node) return;
-
-        dfs(node->left);
-        result.push_back(node->val);
-        dfs(node->right);
-    }
 public:
+    void inorder(TreeNode* root, vector<int>& ans) {
+        if(root == nullptr) return;
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
     vector<int> inorderTraversal(TreeNode* root) {
-        dfs(root);
-        return result;
+        vector<int> ans;
+        inorder(root, ans);
+        return ans;
     }
 };
