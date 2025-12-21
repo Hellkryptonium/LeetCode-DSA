b1dfddc2ec67d7260663b1de2669057643fa03e2
Mohd Harish <harishjs1006@gmail.com>
2025-12-21 21:50:11 +0530

    Time: 0 ms (100%), Space: 12.8 MB (62%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
+    int dp[205][205];
+    bool vis[205][205];
 
-        vector<int> dp = triangle[n-1];
-
-        for(int row = n-2; row >=0; row--) {
-            for(int col = 0; col <= row; col++) {
-                dp[col] = triangle[row][col] + min(dp[col], dp[col+1]);
-            }
+    int solve(int row, int col, vector<vector<int>>& triangle) {
+        if(row == triangle.size() - 1) {
+            return triangle[row][col];
         }
 
-        return dp[0];
+        if(vis[row][col]) {
+            return dp[row][col];
+        }
+
+        int down = solve(row+1, col, triangle);
+        int diag = solve(row+1, col+1, triangle);
+
+        dp[row][col] = triangle[row][col] + min(down, diag);
+        vis[row][col] = true;
+        return dp[row][col];
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        memset(vis, false, sizeof(vis));
+        return solve(0, 0, triangle);
     }
 };
