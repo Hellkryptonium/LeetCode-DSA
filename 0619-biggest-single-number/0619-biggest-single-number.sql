237126160bff5251805f7da00ac9cbb671966c1e
Mohd Harish <harishjs1006@gmail.com>
2026-01-07 16:02:08 +0530

    Time: 400 ms (53.59%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
-with
+with 
     UniqueNumbers as (
-    select num 
-    from MyNumbers
-    group by 1
-    having count(num) = 1
+        select 
+            num
+        from MyNumbers
+        group by 1
+        having count(num) = 1
 )
 select max(num) as num
 from UniqueNumbers;
