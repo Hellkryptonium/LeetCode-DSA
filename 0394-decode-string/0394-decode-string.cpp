8a6da89cc90a96dff1e7f6f1c2f3c6d042083c2a
Mohd Harish <harishjs1006@gmail.com>
2026-01-12 02:45:55 +0530

    Time: 0 ms (100%), Space: 9.5 MB (31.8%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stack<int> numStack;
-        stack<string> strStack;
-        string currStr = "";
+        stack<int> numSt;
+        stack<string> strSt;
         int currNum = 0;
+        string currStr = "";
 
         for(char c : s) {
             if(isdigit(c)) {
-                currNum = currNum * 10 + (c - '0');
-            } else if (c == '[') {
-                numStack.push(currNum);
-                strStack.push(currStr);
+                currNum = currNum * 10 + (c-'0');
+            } else if (c == '['){
+                numSt.push(currNum);
+                strSt.push(currStr);
                 currNum = 0;
                 currStr = "";
             } else if (c == ']') {
-                int repeatTimes = numStack.top(); numStack.pop();
-                string prevStr = strStack.top(); strStack.pop();
-
+                int repeatTimes = numSt.top(); numSt.pop();
+                string prevStr = strSt.top(); strSt.pop();
                 string temp = "";
                 for(int i=0; i<repeatTimes; i++) {
                     temp += currStr;
@@ -27,6 +26,7 @@ public:
                 currStr += c;
             }
         }
+
         return currStr;
     }
 };
