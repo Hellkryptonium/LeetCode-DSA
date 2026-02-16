442d27c6fcd13d495f8318100058a7de3ad74606
Mohd Harish <harishjs1006@gmail.com>
2026-02-16 14:49:01 +0530

    Time: 2 ms (37.23%), Space: 8.2 MB (46.07%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
+    uint32_t reverseBits(int n) {
         uint32_t result = 0;
 
         for(int i=0; i<32; i++) {
@@ -10,6 +10,7 @@ public:
 
             n >>= 1;
         }
+
         return result;
     }
 };
