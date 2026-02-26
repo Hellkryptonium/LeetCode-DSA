2cad7e7183c8a2e2597f496d422416e6c5bdf74f
Mohd Harish <harishjs1006@gmail.com>
2026-02-26 21:03:35 +0530

    Time: 3 ms (70.05%), Space: 13.2 MB (58.33%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,26 +1,67 @@
+class Node {
+public:
+    unordered_map<char, Node*> children;
+    bool endOfWord = false;
+};
+
+class Trie {
+public:
+    Node* root;
+
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+        Node* temp = root;
+        for(char c : word) {
+            if(!temp->children.count(c))
+                temp->children[c] = new Node();
+            temp = temp->children[c];
+        }
+        temp->endOfWord = true;
+    }
+};
+
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.length();
+    bool helper(string &s, int index,
+                Trie &trie,
+                vector<int> &dp) {
 
-        unordered_set<string> words(wordDict.begin(), wordDict.end());
-        vector<bool> dp(n+1, false);
-        dp[0] = true;
+        if(index == s.size())
+            return true;
 
-        int maxLen = 0;
-        for(auto& w : words) {
-            maxLen = max(maxLen, (int)w.length());
-        }
+        if(dp[index] != -1)
+            return dp[index];
 
-        for(int i=1; i<=n; i++) {
-            for(int j=max(0, i-maxLen); j<i; j++) {
-                if(dp[j] && words.count(s.substr(j, i-j))) {
-                    dp[i] = true;
-                    break;
-                }
+        Node* temp = trie.root;
+
+        for(int i = index; i < s.size(); i++) {
+
+            if(!temp->children.count(s[i]))
+                break;
+
+            temp = temp->children[s[i]];
+
+            if(temp->endOfWord &&
+               helper(s, i + 1, trie, dp)) {
+                return dp[index] = true;
             }
         }
 
-        return dp[n];
+        return dp[index] = false;
+    }
+
+    bool wordBreak(string s,
+                   vector<string>& wordDict) {
+
+        Trie trie;
+        for(auto &w : wordDict)
+            trie.insert(w);
+
+        vector<int> dp(s.size(), -1);
+
+        return helper(s, 0, trie, dp);
     }
 };
