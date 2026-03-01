2c5dcfbaff1131b10bf7187016c1401a0de8eab3
Mohd Harish <harishjs1006@gmail.com>
2026-03-01 20:38:00 +0530

    Time: 32 ms (5.04%), Space: 18.5 MB (48.31%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,40 +1,77 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for(auto& pre : prerequisites) {
-            adj[pre[1]].push_back(pre[0]);
+
+    bool isCycle(int src,
+                 vector<vector<int>>& adj,
+                 vector<bool>& vis,
+                 vector<bool>& rec)
+    {
+        vis[src] = true;
+        rec[src] = true;
+
+        for (int v : adj[src]) {
+
+            if (!vis[v]) {
+                if (isCycle(v, adj, vis, rec))
+                    return true;
+            }
+            else if (rec[v]) {
+                return true;
+            }
         }
 
-        vector<int> visited(numCourses, 0);
-        vector<int> order;
-        bool hasCycle = false;
+        rec[src] = false;   // backtrack
+        return false;
+    }
 
-        function<void(int)> dfs = [&](int node) {
-            if(hasCycle) return;
-            visited[node] = 1;
+    void topoSort(int src, vector<bool> &vis, stack<int>& s, vector<vector<int>>& graph) {
+        vis[src] = true;
 
-            for(int next : adj[node]) {
-                if(visited[next] == 0) {
-                    dfs(next);
-                } else if(visited[next] == 1) {
-                    hasCycle = true;
+        for(int i=0; i<graph.size(); i++) {
+            int u = graph[i][1];
+            int v = graph[i][0];
+
+            if(u == src) {
+                if(!vis[v]) {
+                    topoSort(v, vis, s, graph);
                 }
             }
+        }
+        s.push(src);
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int V = numCourses;
+        vector<vector<int>> adj(numCourses);
 
-            visited[node] = 2;
-            order.push_back(node);
-        };
+        // build graph
+        for (auto &e : prerequisites) {
+            adj[e[1]].push_back(e[0]);
+        }
 
-        for(int i=0; i<numCourses; i++) {
-            if(visited[i] == 0) {
-                dfs(i);
+        vector<bool> vis(numCourses, false);
+        vector<bool> rec(numCourses, false);
+        vector<int> ans;
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!vis[i]) {
+                if (isCycle(i, adj, vis, rec))
+                    return ans;
             }
         }
 
-        if(hasCycle) return {};
-        reverse(order.begin(), order.end());
+        //topological sortingvis
+        vector<bool> vis2(V, false);
+        stack<int> s;
+        for(int i=0; i<V; i++) {
+            if(!vis2[i]) {
+                topoSort(i, vis2, s, prerequisites);
+            }
+        }
 
-        return order;
+        while(s.size() > 0) {
+            ans.push_back(s.top());
+            s.pop();
+        }
+        return ans;
     }
 };
